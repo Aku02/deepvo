004abea96602c909c9aa1763947eb633b595ab0f
Shrinath Deshpande <shrinath.deshpande@stonybrook.edu>
2017-11-22 18:04:50 -0500

    Replaced deepVO object with simlar code structure, code for Session is written which has one issue

--- main.py
+++ main.py
@@ -6,18 +6,29 @@ import cv2
 import math
 import warnings
 
-# Checks if a matrix is a valid rotation matrix.
+
+""" Hyper Parameters for learning"""
+LEARNING_RATE = 0.001
+BATCH_SIZE = 1
+LSTM_HIDDEN_SIZE = 1000
+LSTM_NUM_LAYERS = 2
+NUM_TRAIN_STEPS = 1000
+
+
 def isRotationMatrix(R):
-    """ referred from https://www.learnopencv.com/rotation-matrix-to-euler-angles/ """
+    """ Checks if a matrix is a valid rotation matrix
+        referred from https://www.learnopencv.com/rotation-matrix-to-euler-angles/
+    """
     Rt = np.transpose(R)
     shouldBeIdentity = np.dot(Rt, R)
     I = np.identity(3, dtype = R.dtype)
     n = np.linalg.norm(I - shouldBeIdentity)
     return n < 1e-6
 
-# Calculates rotation matrix to euler angles
 def rotationMatrixToEulerAngles(R):
-    """ referred from https://www.learnopencv.com/rotation-matrix-to-euler-angles/ """
+    """ calculates rotation matrix to euler angles
+        referred from https://www.learnopencv.com/rotation-matrix-to-euler-angles
+    """
     assert(isRotationMatrix(R))
     sy = math.sqrt(R[0,0] * R[0,0] +  R[1,0] * R[1,0])
     singular = sy < 1e-6
@@ -33,202 +44,214 @@ def rotationMatrixToEulerAngles(R):
 
     return np.array([x, y, z])
 
-# deepVO class
-class deepVO(object):
-    """ deepVO RCNN Model """
+def get_lstm_cell(config, is_training):
+    return tf.contrib.rnn.BasicLSTMCell(
+            config.hidden_size,
+            forget_bias=1.0)
 
-    def __init__(self, config, is_training=True, _only_position = True, num_frames=100):
-        """ Initialization
-        param: config = configuration
-        param: dataset = KITTY Dataset 0th trajectory
-            dataset structure:
+def build_rcnn_graph(config, input_ ,is_training):
+    """ CNN layers connected to RNN which connects to final output """
+
+    # Slightly better results can be obtained with forget gate biases
+    # initialized to 1 but the hyperparameters of the model would need to be
+    # different than reported in the paper.
+    cell = get_lstm_cell(config, is_training)
+    # create 2 LSTMCells
+    rnn_layers = [tf.nn.rnn_cell.LSTMCell(size) for size in [config.hidden_size, config.hidden_size]]
+
+    # create a RNN cell composed sequentially of a number of RNNCells
+    multi_rnn_cell = tf.nn.rnn_cell.MultiRNNCell(rnn_layers)
+
+    rnn_inputs = [cnn_layers(stacked_img) for \
+            stacked_img in input_]
+
+    # Flattening the final convolution layers to feed them into RNN
+    rnn_inputs = [tf.reshape(rnn_inputs[i],[-1, 20*6*1024]) for i in range(len(rnn_inputs))]
+
+    max_time = len(rnn_inputs)
+    rnn_inputs = tf.convert_to_tensor(rnn_inputs)
+    rnn_inputs = tf.reshape(rnn_inputs, [config.batch_size, max_time, 20*6*1024])
+    # 'outputs' is a tensor of shape [batch_size, max_time, 1000]
+    # 'state' is a N-tuple where N is the number of LSTMCells containing a
+    # tf.contrib.rnn.LSTMStateTuple for each cell
+    outputs, state = tf.nn.dynamic_rnn(cell=multi_rnn_cell,
+                                       inputs=rnn_inputs,
+                                       dtype=tf.float32)
+    return outputs, state
+
+def cnn_layers(input_layer):
+        """ input: input_layer of concatonated images (img, img_next) where \
+                shape of each imgae is (1280, 384, 3)
+            output: 6th convolutional layer
+            The structure of the CNN is inspired by the network for optical flow estimation
+                in A. Dosovitskiy, P. Fischery, E. Ilg, C. Hazirbas, V. Golkov, P. van der
+                Smagt, D. Cremers, T. Brox et al Flownet: Learning optical flow
+                with convolutional networks, in Proceedings of IEEE International
+                Conference on Computer Vision (ICCV)
         """
-        # configuration
-        self._config = config
-
-        # Importing stacked images and ground truth
-        # stacked images shape: [height, width, channels = 2]
-        # pose_ground_truth shape: [num_frames, 12]
-        self._input_images_stacked, pose_ground_truth = self._import_dataset(num_frames)
-
-        #consinder only position
-        #_only_position = True
-
-        if not _only_position:
-            pose_size = 6
-            rotation_matrix = pose_ground_truth[:,:9]
-            rotation_matrix = np.reshape(rotation_matrix[:,:9], [-1, 3, 3])
-            self._pose_ground_truth = np.zeros((pose_ground_truth.shape[0], pose_size))
-            self._pose_ground_truth[:,:3] = pose_ground_truth[:,9:12]
-            for i in range(rotation_matrix.shape[0]):
-                self._pose_ground_truth[i,3:6] = rotationMatrixToEulerAngles(rotation_matrix[i])
-        else:
-            pose_size = 3
-            self._pose_ground_truth = pose_ground_truth[:,9:12]
-            warnings.warn("Warning! Orientation data is ignored!")
-
-        (output, _)  = self._build_rnn_graph(is_training)
-
-        # Change the below code for different loss function based on paper
-        regression_w = tf.get_variable('regression_w', shape=[self._config.hidden_size, pose_size], dtype=tf.float32)
-        regression_b = tf.get_variable("regression_b", shape=[pose_size], dtype=tf.float32)
-
-        pose_estimated = [tf.nn.xw_plus_b(output[i], regression_w, regression_b) for i in range(output.shape[0])]
-        pose_estimated = tf.reshape(tf.convert_to_tensor(pose_estimated), [num_frames, pose_size])
-        # Use the contrib sequence loss and average over the batches
-        self._loss = tf.reduce_sum(tf.square(pose_estimated - self._pose_ground_truth[:num_frames,:]))
-
-    def _import_dataset(self, num_frames=None):
-        """ Function that loads dataset
-        """
-        if num_frames == None:
-            num_frames = 4500
-
-        trajectory = 0
-        img_stacked = []
-        with open('/Users/Shrinath/visual-odometry/dataset/poses/' +  '%02d' % trajectory + '.txt') as f:
-            poses_ground_truth = np.array([[float(x) for x in line.split()] for line in f])
-        for i in range(num_frames):
-            img1 = cv2.imread('/Users/Shrinath/visual-odometry/dataset/sequences/'+ '%02d' % trajectory + '/image_0/' +  '%06d' % i + '.png', 0)
-            img2 = cv2.imread('/Users/Shrinath/visual-odometry/dataset/sequences/00/image_0/' + '%06d' % (i+1) + '.png', 0)
-            width, height = img1.shape
-            img1 = tf.cast(img1, dtype=tf.float32)
-            img2 = tf.cast(img2, dtype=tf.float32)
-            img_aug = tf.stack([img1, img2], axis=2)
-            img_aug = tf.reshape(img_aug, (1, height, width, 2))
-            img_stacked.append(img_aug)
-        return img_stacked, poses_ground_truth
-
-    def _get_lstm_cell(self, is_training):
-        return tf.contrib.rnn.BasicLSTMCell(
-                self._config.hidden_size,
-                forget_bias=1.0)
-
-    def _build_rnn_graph(self, is_training):
-        """ RNN layers which connects to final output
-            Build the inference graph using canonical LSTM cells.
-        """
-        # Slightly better results can be obtained with forget gate biases
-        # initialized to 1 but the hyperparameters of the model would need to be
-        # different than reported in the paper.
-        cell = self._get_lstm_cell(is_training)
-        # create 2 LSTMCells
-        rnn_layers = [tf.nn.rnn_cell.LSTMCell(size) for size in [self._config.hidden_size, self._config.hidden_size]]
-
-        # create a RNN cell composed sequentially of a number of RNNCells
-        multi_rnn_cell = tf.nn.rnn_cell.MultiRNNCell(rnn_layers)
-
-        rnn_inputs = [self._cnn_layers(stacked_img) for \
-                stacked_img in self._input_images_stacked]
-        rnn_inputs = [tf.reshape(rnn_inputs[i],[-1, 20*6*1024]) for i in range(len(rnn_inputs))]
-        max_time = len(rnn_inputs)
-        rnn_inputs = tf.convert_to_tensor(rnn_inputs)
-        rnn_inputs = tf.reshape(rnn_inputs, [self._config.batch_size, max_time, 20*6*1024])
-        # 'outputs' is a tensor of shape [batch_size, max_time, 1000]
-        # 'state' is a N-tuple where N is the number of LSTMCells containing a
-        # tf.contrib.rnn.LSTMStateTuple for each cell
-        outputs, state = tf.nn.dynamic_rnn(cell=multi_rnn_cell,
-                                           inputs=rnn_inputs,
-                                           dtype=tf.float32)
-        return outputs, state
-
-    def _cnn_layers(self, input_layer):
-            """ input: input_layer of concatonated images (img, img_next) where \
-                    shape of each imgae is (1280, 384, 3)
-                output: 6th convolutional layer
-                The structure of the CNN is inspired by the network for optical flow estimation
-                    in A. Dosovitskiy, P. Fischery, E. Ilg, C. Hazirbas, V. Golkov, P. van der
-                    Smagt, D. Cremers, T. Brox et al Flownet: Learning optical flow
-                    with convolutional networks, in Proceedings of IEEE International
-                    Conference on Computer Vision (ICCV)
-            """
-            # input_layer size [1280, 384, 6]
-            # Convolutional Layer #1
-            # Computes 32 features using a 7x7 filter with ReLU activation.
-            # Padding is added to preserve width and height.
-            # Input Tensor Shape: [batch_size, 1280, 384, 1]
-            # Output Tensor Shape: [batch_size, 1280, 384, 32]
-            conv1 = tf.layers.conv2d(
-                inputs=input_layer,
-                filters=64,
-                kernel_size=[7, 7],
-                padding="same",
+        # input_layer size [1280, 384, 6]
+        # Convolutional Layer #1
+        # Computes 32 features using a 7x7 filter with ReLU activation.
+        # Padding is added to preserve width and height.
+        # Input Tensor Shape: [batch_size, 1280, 384, 1]
+        # Output Tensor Shape: [batch_size, 1280, 384, 32]
+        conv1 = tf.layers.conv2d(
+            inputs=input_layer,
+            filters=64,
+            kernel_size=[7, 7],
+            padding="same",
+            strides=2,
+            activation=tf.nn.relu)
+        # Pooling Layer #1
+        # First max pooling layer with a 2x2 filter and stride of 2
+        # Input Tensor Shape: [batch_size, 1280, 384, 64]
+        # Output Tensor Shape: [batch_size, 640, 192, 64]
+        conv2 = tf.layers.conv2d(
+                inputs=conv1,
+                filters=128,
+                kernel_size=[5, 5],
+                padding ="same",
                 strides=2,
                 activation=tf.nn.relu)
-            # Pooling Layer #1
-            # First max pooling layer with a 2x2 filter and stride of 2
-            # Input Tensor Shape: [batch_size, 1280, 384, 64]
-            # Output Tensor Shape: [batch_size, 640, 192, 64]
-            conv2 = tf.layers.conv2d(
-                    inputs=conv1,
-                    filters=128,
-                    kernel_size=[5, 5],
-                    padding ="same",
-                    strides=2,
-                    activation=tf.nn.relu)
-            conv3 = tf.layers.conv2d(
-                    inputs=conv2,
-                    filters=256,
-                    kernel_size=[5, 5],
-                    padding ="same",
-                    strides=2,
-                    activation=tf.nn.relu)
-            conv3_1 = tf.layers.conv2d(
-                    inputs=conv3,
-                    filters=256,
-                    kernel_size=[3, 3],
-                    padding ="same",
-                    strides=1,
-                    activation=tf.nn.relu)
-            conv4 = tf.layers.conv2d(
-                    inputs=conv3_1,
-                    filters=512,
-                    kernel_size=[3, 3],
-                    padding ="same",
-                    strides=2,
-                    activation=tf.nn.relu)
-            conv4_1 = tf.layers.conv2d(
-                    inputs=conv4,
-                    filters=512,
-                    kernel_size=[3, 3],
-                    padding ="same",
-                    strides=1,
-                    activation=tf.nn.relu)
-            conv5 = tf.layers.conv2d(
-                    inputs=conv4_1,
-                    filters=512,
-                    kernel_size=[3, 3],
-                    padding ="same",
-                    strides=2,
-                    activation=tf.nn.relu)
-            conv5_1 = tf.layers.conv2d(
-                    inputs=conv5,
-                    filters=512,
-                    kernel_size=[3, 3],
-                    padding ="same",
-                    strides=1,
-                    activation=tf.nn.relu)
-            output = tf.layers.conv2d(
-                    inputs=conv5_1,
-                    filters=1024,
-                    kernel_size=[3, 3],
-                    padding ="same",
-                    strides=2)
-            """ The output is connected to RNN
-            """
-            return output
+        conv3 = tf.layers.conv2d(
+                inputs=conv2,
+                filters=256,
+                kernel_size=[5, 5],
+                padding ="same",
+                strides=2,
+                activation=tf.nn.relu)
+        conv3_1 = tf.layers.conv2d(
+                inputs=conv3,
+                filters=256,
+                kernel_size=[3, 3],
+                padding ="same",
+                strides=1,
+                activation=tf.nn.relu)
+        conv4 = tf.layers.conv2d(
+                inputs=conv3_1,
+                filters=512,
+                kernel_size=[3, 3],
+                padding ="same",
+                strides=2,
+                activation=tf.nn.relu)
+        conv4_1 = tf.layers.conv2d(
+                inputs=conv4,
+                filters=512,
+                kernel_size=[3, 3],
+                padding ="same",
+                strides=1,
+                activation=tf.nn.relu)
+        conv5 = tf.layers.conv2d(
+                inputs=conv4_1,
+                filters=512,
+                kernel_size=[3, 3],
+                padding ="same",
+                strides=2,
+                activation=tf.nn.relu)
+        conv5_1 = tf.layers.conv2d(
+                inputs=conv5,
+                filters=512,
+                kernel_size=[3, 3],
+                padding ="same",
+                strides=1,
+                activation=tf.nn.relu)
+        output = tf.layers.conv2d(
+                inputs=conv5_1,
+                filters=1024,
+                kernel_size=[3, 3],
+                padding ="same",
+                strides=2)
+        """ The output is connected to RNN
+        """
+        return output
 
-# config class
+def import_dataset(num_frames=None, trajectory=0):
+    """ Function that loads dataset
+    """
+    if num_frames == None:
+        num_frames = 4500
+    img1 = cv2.imread('/Users/Shrinath/visual-odometry/dataset/sequences/'+ '%02d' % trajectory + '/image_0/' +  '%06d' % 0 + '.png', 0)
+    height, width = img1.shape
+    img_stacked = np.zeros((num_frames, 1, height, width, 2))
+    with open('/Users/Shrinath/visual-odometry/dataset/poses/' +  '%02d' % trajectory + '.txt') as f:
+        poses_ground_truth = np.array([[float(x) for x in line.split()] for line in f])
+    for i in range(num_frames):
+        img1 = cv2.imread('/Users/Shrinath/visual-odometry/dataset/sequences/'+ '%02d' % trajectory + '/image_0/' +  '%06d' % i + '.png', 0)
+        img2 = cv2.imread('/Users/Shrinath/visual-odometry/dataset/sequences/'+ '%02d' % trajectory + '/image_0/' +  '%06d' % (i+1) + '.png', 0)
+        img1 = np.reshape(img1, [height, width, 1])
+        img2 = np.reshape(img2, [height, width, 1])
+        img_aug = np.concatenate([img1, img2], axis=2)
+        img_stacked[i, 0, :, :, :] = img_aug
+    return img_stacked, poses_ground_truth
+
+# Config class
 class Config(object):
-    def __init__(self, lstm_hidden_size=1000, lstm_num_layers=2, batch_size=1, num_steps= 20):
+    """configuration of RNN """
+    def __init__(self, lstm_hidden_size=1000, lstm_num_layers=2, batch_size=1, num_steps= 20, learning_rate=0.001, only_position=True):
         self.hidden_size = lstm_hidden_size
         self.num_layers = lstm_num_layers
         self.batch_size = batch_size
         self.num_steps = num_steps
+        self.learning_rate = learning_rate
+        self.only_position = only_position
 
-def main():
-    config = Config(lstm_hidden_size=1000, lstm_num_layers=2)
-    vo_train = deepVO(config)
+def main(num_frames=10, is_training=True):
+    config = Config(lstm_hidden_size=LSTM_HIDDEN_SIZE, lstm_num_layers=LSTM_NUM_LAYERS)
+    # Initialize the variables (i.e. assign their default value)
+    init = tf.global_variables_initializer()
+    # configuration
+    timesteps = num_frames
+    if not config.only_position:
+        pose_size = 6
+    else:
+        pose_size = 3
+        warnings.warn("Warning! Orientation data is ignored!")
+
+    # only for gray scale dataset, for colored channels will be 6
+    height, width, channels = 376, 1241, 2
+
+    # placeholder for input
+    input_ = tf.placeholder(tf.float32, [timesteps, None, height, width, channels])
+    input_ = tf.unstack(input_, timesteps, 0)
+    # placeholder for labels
+    labels_ = tf.placeholder(tf.float32, [None, pose_size])
+
+    # Building the RCNN Network
+    (output, _)  = build_rnn_graph(config, input_, is_training)
+
+    # Output layer to compute the output
+    regression_w = tf.get_variable('regression_w', shape=[config.hidden_size, pose_size], dtype=tf.float32)
+    regression_b = tf.get_variable("regression_b", shape=[pose_size], dtype=tf.float32)
+
+    # Pose estimate by multiplication with RCNN_output and Output layer
+    pose_estimated = [tf.nn.xw_plus_b(output[i], regression_w, regression_b) for i in range(output.shape[0])]
+    pose_estimated = tf.reshape(tf.convert_to_tensor(pose_estimated), [num_frames, pose_size])
+
+    print pose_estimated.shape
+    # Loss function for all the frames in a batch
+    loss_op = tf.reduce_sum(tf.square(pose_estimated - labels_[:num_frames,:]))
+
+    #optimizer
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=config.learning_rate)
+    train_op = optimizer.minimize(loss_op)
+
+    # Start training
+    with tf.Session() as sess:
+        # Run the initializer
+        sess.run(init)
+        for step in range(1, config.num_steps+1):
+            batch_x, batch_y = import_dataset(num_frames=100)
+            print batch_x.shape
+            # Run optimization op (backprop)
+            sess.run(train_op, feed_dict={input_: batch_x, labels_: batch_y})
+            if step % 200 == 0 or step == 1:
+                # Calculate batch loss and accuracy
+                loss = sess.run(loss_op, feed_dict={input_: batch_x,
+                    labels_: batch_y})
+                print("Step " + str(step) + ", Minibatch Loss= " + \
+                      "{:.4f}".format(loss) + ", Training Accuracy= ")
+
+        print("Optimization Finished!")
 
 if __name__ == "__main__":
     main()
