2710cbd76cd1a897163df6efba4ff1d74f6639c3
Shrinath Deshpande <shrinath.deshpande@stonybrook.edu>
2017-12-16 03:53:46 -0500

    axis equal

--- src/plot.py
+++ src/plot.py
@@ -26,16 +26,18 @@ def parseFile():
 	fp.close()
 	ground, = plt.plot(x_g, z_g, label = 'ground')
 	est , = plt.plot(x_e, z_e, label = 'estimate')
+        plt.axis('equal')
 	plt.legend([ground, est], ['ground', 'estimate'])
+        plt.show(block=False)
 	fp = open('img_file_names', "r")
 	for line in fp:
 		split = line.split()
 		img = cv2.imread(split[0],0)
 		cv2.imshow('image',img)
-		cv2.waitKey(1)
+		cv2.waitKey(3)
 	cv2.destroyAllWindows()
 	plt.show()
 
 if __name__ == "__main__":
     parseFile()
-    
+
