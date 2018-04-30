ef5d51d0ae9d3a62c468636322244e032a88c372
Shrinath Deshpande <shrinath.deshpande@stonybrook.edu>
2018-04-29 22:07:18 -0400

    removed tabs

--- src/plot.py
+++ src/plot.py
@@ -10,34 +10,33 @@ y_e = []
 z_e = []
 
 def parseFile():
-	fp = open('output_file', "r")
-	for line in fp:
-		split = line.split()
-		x_g.append(split[0])
-		y_g.append(split[1])
-		z_g.append(split[2])
-	fp.close()
-	fp = open('estimated', "r")
-	for line in fp:
-		split = line.split()
-		x_e.append(split[0])
-		y_e.append(split[1])
-		z_e.append(split[2])
-	fp.close()
-	ground, = plt.plot(x_g, z_g, label = 'ground')
-	est , = plt.plot(x_e, z_e, label = 'estimate')
-        plt.axis('equal')
-	plt.legend([ground, est], ['ground', 'estimate'])
-        plt.show(block=False)
-	fp = open('img_file_names', "r")
-	for line in fp:
-		split = line.split()
-		img = cv2.imread(split[0],0)
-		cv2.imshow('image',img)
-		cv2.waitKey(3)
-	cv2.destroyAllWindows()
-	plt.show()
+    fp = open('output_file', "r")
+    for line in fp:
+        split = line.split()
+        x_g.append(split[0])
+        y_g.append(split[1])
+        z_g.append(split[2])
+    fp.close()
+    fp = open('estimated', "r")
+    for line in fp:
+        split = line.split()
+        x_e.append(split[0])
+        y_e.append(split[1])
+        z_e.append(split[2])
+    fp.close()
+    ground, = plt.plot(x_g, z_g, label = 'ground')
+    est , = plt.plot(x_e, z_e, label = 'estimate')
+    plt.axis('equal')
+    plt.legend([ground, est], ['ground', 'estimate'])
+    plt.show(block=False)
+    fp = open('img_file_names', "r")
+    for line in fp:
+        split = line.split()
+        img = cv2.imread(split[0],0)
+        cv2.imshow('image',img)
+        cv2.waitKey(3)
+    cv2.destroyAllWindows()
+    plt.show()
 
 if __name__ == "__main__":
     parseFile()
-
